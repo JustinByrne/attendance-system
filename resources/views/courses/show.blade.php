a4a8ec3d3eaa21799dcb0e8e26a804968683b65b
Justin Byrne <theskaterjesus@gmail.com>
2022-03-22 18:47:46 +0000

    Added the dates of the course to the show page

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ $course->name }} - {{ $course->start_date->diffInDays($course->end_date) }} days
+            {{ $course->name }} ... {{ $course->start_date->format("d/m/y") }} - {{ $course->end_date->format("d/m/y") }}
         </h2>
     </x-slot>
 
