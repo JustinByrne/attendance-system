e91ab4afbfc4fb48373f18b8cd31161bb7f66fa8
Justin Byrne <theskaterjesus@gmail.com>
2022-03-27 19:17:10 +0100

    Added page to take the register

---
+++ resources/views/courses/register.blade.php
@@ -0,0 +1,44 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Course Registration') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <form action="{{ route('courses.storeRegister', $course) }}" method="POST">
+                        @csrf
+                        <input type="date" name="attendance_date" required>
+                        <table class="w-full table-fixed">
+                            <thead>
+                                <tr>
+                                    <th class="text-left px-2">Learner name</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($course->learners as $learner)
+                                    <tr class="odd:bg-white even:bg-slate-50">
+                                        <td class="px-2 py-3">{{ $learner->name }}</td>
+                                        <td>
+                                            <select @if ($loop->first) autofocus @endif name="attendance[{{ $learner->id }}]" required>
+                                                <option selected disabled value="">-</option>
+                                                @foreach ($statuses as $status)
+                                                    <option value="{{ $status->id }}">{{ $status->code }}</option>
+                                                @endforeach
+                                            </select>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        <x-button>Submit</x-button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
