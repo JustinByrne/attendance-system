7c6910e602667554a5db1a701101a7a108fd0d17
Justin Byrne <theskaterjesus@gmail.com>
2022-03-27 18:50:46 +0100

    Added a button for the take register route

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -26,6 +26,7 @@
             </div>
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                    <a href="{{ route('courses.register', $course) }}" class="float-right mb-4 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">Take register</a>
                     <table class="w-full table-fixed">
                         <thead>
                             <tr>
