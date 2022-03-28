87908f6f05eb252bc08bb0db73534bd9bdd33eb1
Justin Byrne <theskaterjesus@gmail.com>
2022-03-28 19:10:18 +0100

    Added form request to check the provided data is valid

---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "attendance_date" => "required",
+            "attendance" => "required|array",
+            "attendance.*" => "required|array|exists:App\Models\Learner,id",
+            "attendance.*.status" =>
+                "required|exists:App\Models\AttendanceStatus,id",
+        ];
+    }
+}
