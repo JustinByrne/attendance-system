d60688da4c5172b0f8a556844d67d8c79db99e80
Justin Byrne <theskaterjesus@gmail.com>
2022-04-01 19:01:15 +0100

    Added button to download register

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -26,6 +26,10 @@
             </div>
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                    <div class="float-right">
+                        <a href="{{ route('courses.register', $course) }}" class="mb-4 mr-4 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">Take register</a>
+                        <a href="{{ route('courses.register.download', $course) }}" class="mb-4 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">Download</a>
+                    </div>
                     @include("courses.table")
                 </div>
             </div>
